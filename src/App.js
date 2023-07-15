8724206db193357dff20792653e15b150e832bfa
Erik Schwarz <erik.schwarz06@gmail.com>
2023-07-15 18:42:00 +0200

    created class to create user instead of doing it with consts only

--- src/App.js
+++ src/App.js
@@ -3,72 +3,72 @@ import "./App.css";
 import React, { useState } from "react";
 
 class User {
-  constructor(name, imageUrl, imageSize) {
-    this.name = name;
-    this.imageUrl = imageUrl;
-    this.imageSize = imageSize;
-  }
+    constructor(name, imageUrl, imageSize) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.imageSize = imageSize;
+    }
 }
 
-const user = new User("Hedy Lamarr","https://i.imgur.com/yXOvdOSs.jpg",90);
+const user = new User("Hedy Lamarr", "https://i.imgur.com/yXOvdOSs.jpg", 90);
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  const handleLogout = () => {
-    setIsLoggedIn(!isLoggedIn);
-  };
+    const handleLogout = () => {
+        setIsLoggedIn(!isLoggedIn);
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {isLoggedIn ? (
-          <>
-            <Profile user={user} />
-            <button onClick={handleLogout}>Logout</button>
-          </>
-        ) : (
-          <>
-            <LoginForm />
-            <button onClick={handleLogout}>Login</button>
-          </>
-        )}
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                {isLoggedIn ? (
+                    <>
+                        <Profile user={user} />
+                        <button onClick={handleLogout}>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <LoginForm />
+                        <button onClick={handleLogout}>Login</button>
+                    </>
+                )}
+            </header>
+        </div>
+    );
 }
 
 function Profile({ user }) {
-  return (
-    <div>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={"Photo of " + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize,
+                }}
+            />
+        </div>
+    );
 }
 
 function LoginForm() {
-  return <></>;
+    return <></>;
 }
 
 export default App;
