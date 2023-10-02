d7f540e81781147733f004e281b72ca64eb5e0ed
Erik Schwarz <erik.schwarz06@gmail.com>
2023-10-02 16:29:55 +0200

    First complete HomePage with BB stuff

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import BB_Logo_W from '../../assets/images/BB_Logo_W.svg'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import {
   faGithub,
@@ -11,8 +12,8 @@ import {
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
-      {/* <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="logo subtitle" /> */}
+      <img src={BB_Logo_W} alt="logo" />
+      {/* <img className="sub-logo" src={LogoSubtitle} alt="logo subtitle" /> */}
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
